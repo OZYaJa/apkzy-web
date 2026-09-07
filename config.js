c22c36bcc84c40344bcc4414abd3449fd55ada89
OZYaJa <ozyaja064@gmail.com>
2026-09-07 20:08:11 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+/* =========================================================
+   TOOLSZY PUBLIC WEBSITE
+   config.js
+   Konfigurasi GitHub + Supabase
+   ========================================================= */
+
+window.APKZY_CONFIG = {
+
+  /* =========================
+     SUPABASE
+     ========================= */
+
+  SUPABASE_URL: "ISI_SUPABASE_URL",
+
+  SUPABASE_ANON_KEY: "ISI_SUPABASE_ANON_KEY",
+
+  /* =========================
+     DATABASE
+     ========================= */
+
+  TABLE: "apps",
+
+  /* Kolom jumlah download */
+  DOWNLOAD_FIELD: "download_count"
+
+};
